b2aa4b0db1650abf8cce7cabfaab5b959c944026
Rehan Mahmood <git@rehan.appaddy.uk>
2024-12-01 16:28:04 -0500

    Added order processing ack publishing support on another topic.

--- src/main/scala/Orchestrator.scala
+++ src/main/scala/Orchestrator.scala
@@ -1,5 +1,6 @@
 package aiven.kafka
 
+import cats.implicits.*
 import aiven.kafka.settings.AppSettings
 import cats.effect.{ Async, Resource, Sync }
 import cats.effect.kernel.Concurrent
@@ -17,18 +18,33 @@ case object Orchestrator {
     settings: AppSettings.ConsumerSettings
   )(implicit F: Sync[F]): Resource[F, ConsumerSettings[F, String, String]] =
     Resource.pure {
-      ConsumerSettings(
-        keyDeserializer = Deserializer[F, String],
-        valueDeserializer = Deserializer[F, String]
-      ).withAutoOffsetReset(AutoOffsetReset.Earliest)
+      ConsumerSettings[F, String, String]
+        .withAutoOffsetReset(AutoOffsetReset.Earliest)
         .withBootstrapServers(settings.bootstrapServers)
         .withGroupId(settings.group)
         .withProperties(
           "security.protocol"       -> settings.authentication.securityProtocol,
-          "ssl.truststore.location" -> settings.authentication.truststoreLocation, // ".secrets/client.truststore.jks",
-          "ssl.truststore.password" -> settings.authentication.truststorePassword, // "aiven-test",
+          "ssl.truststore.location" -> settings.authentication.truststoreLocation,
+          "ssl.truststore.password" -> settings.authentication.truststorePassword,
           "ssl.keystore.type"       -> settings.authentication.keystoreType,
-          "ssl.keystore.location"   -> settings.authentication.keystoreLocation,   // ".secrets/client.keystore.p12",
+          "ssl.keystore.location"   -> settings.authentication.keystoreLocation,
+          "ssl.keystore.password"   -> settings.authentication.keystorePassword,
+          "ssl.key.password"        -> settings.authentication.keyPassword
+        )
+    }
+
+  private def createProducerSettings[F[_]](
+    settings: AppSettings.ProducerSettings
+  )(implicit F: Sync[F]): Resource[F, ProducerSettings[F, String, String]] =
+    Resource.pure {
+      ProducerSettings[F, String, String]
+        .withBootstrapServers(settings.bootstrapServers)
+        .withProperties(
+          "security.protocol"       -> settings.authentication.securityProtocol,
+          "ssl.truststore.location" -> settings.authentication.truststoreLocation,
+          "ssl.truststore.password" -> settings.authentication.truststorePassword,
+          "ssl.keystore.type"       -> settings.authentication.keystoreType,
+          "ssl.keystore.location"   -> settings.authentication.keystoreLocation,
           "ssl.keystore.password"   -> settings.authentication.keystorePassword,
           "ssl.key.password"        -> settings.authentication.keyPassword
         )
@@ -36,26 +52,55 @@ case object Orchestrator {
 
   def create[F[_]: Concurrent](implicit F: Async[F]): Resource[F, Orchestrator[F]] =
     for {
-      appSettings      <- AppSettings.load
-      consumerConfig   <- Resource.pure(appSettings.aiven.stream.consumer)
-      consumerSettings <- createConsumerSettings(consumerConfig)
-      kafkaTopic       <- Resource.pure(consumerConfig.topic)
+      appSettings         <- AppSettings.load
+      consumerConfig      <- Resource.pure(appSettings.aiven.stream.consumer)
+      consumerSettings    <- createConsumerSettings(consumerConfig)
+      producerConfig      <- Resource.pure(appSettings.aiven.stream.producer)
+      producerSettings    <- createProducerSettings(producerConfig)
+      pizzaOrdersTopic    <- Resource.pure(consumerConfig.topic)
+      pizzaOrdersAckTopic <- Resource.pure(producerConfig.topic)
     } yield new Orchestrator[F] {
       override def run: Stream[F, Unit] = {
 
-        def processRecord(record: ConsumerRecord[String, String]): F[Unit] =
-          F.delay(println(s"Processing record: $record"))
+        def processRecord(
+          committable: CommittableConsumerRecord[F, String, String]
+        ): F[ProducerRecord[String, String]] =
+          for {
+            key        <- F.delay(committable.record.key)
+            value      <- F.delay(committable.record.value)
+            ackMessage <- F.delay {
+                            s"""
+                               |{
+                               |  "status": "processed",
+                               |  "source": {
+                               |    "topic": "${committable.offset.topicPartition.topic()}",
+                               |    "partition": ${committable.offset.topicPartition.partition()},
+                               |    "offset": ${committable.offset.offsetAndMetadata.offset()}
+                               |  }
+                               |}
+                               |""".stripMargin
+                          }
+            ackRecord  <- F.delay(ProducerRecord(pizzaOrdersAckTopic, key, value = ackMessage))
+            _          <- F.delay(println(s"Processing record key: $key with payload $value"))
+            _          <- F.delay(println(s"Publishing Ack: $key with payload $ackMessage"))
+          } yield ackRecord
 
         KafkaConsumer
           .stream(consumerSettings)
-          .subscribeTo(kafkaTopic)
+          .subscribeTo(pizzaOrdersTopic)
           .partitionedRecords
           .map { partitionStream =>
-            partitionStream.evalMap { committable =>
-              processRecord(committable.record)
-            }
+            partitionStream
+              .evalMap { committable =>
+                processRecord(committable)
+              }
+              .map { record =>
+                ProducerRecords.one(record)
+              }
+              .through(KafkaProducer.pipe(producerSettings))
           }
           .parJoinUnbounded
+          .as(F.unit)
       }
     }
 }
